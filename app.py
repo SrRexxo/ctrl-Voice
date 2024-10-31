fab74e929bdc253c7404e0e35c972179e2abdf8a
SrRexxo <133136624+SrRexxo@users.noreply.github.com>
2024-10-31 06:53:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,11 @@ image = Image.open('voice_ctrl.jpg')
 st.image(image, width=200)
 
 
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+local_css("style.css")
 
 st.write("Toca el Botón y habla ")
 
